6ae4423263091318cf449c8b1429a0bcda959929
LiteCloud2022 <litecloud2017@gmail.com>
2023-04-27 19:44:44 +0300

    Доделал

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -2,11 +2,59 @@
 {
     public class Exercise1
     {
-
+        public void DoSequence(int N)
+        { 
+            for (int n = 0; n < N; n++)
+            {
+                if ((n+1) == N)
+                {
+                    Console.Write((n + 1) + ".");
+                }
+                else
+                {
+                    Console.Write((n+1) + ", ");
+                }
+            }
+            Console.WriteLine("");
+        }
     }
 
     public class Exercise2
     {
-
+        public void DoRectangle(int N)
+        {
+            if ((N % 2) == 1)
+            {
+                for (int n = 0; n < N; n++)
+                {
+                    if ((N/2) == n)
+                    {
+                        for (int x = 0; x < N; x++)
+                        {
+                            if ((N / 2) == x)
+                            {
+                                Console.Write(" ");
+                            }
+                            else
+                            {
+                                Console.Write("*");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int x = 0; x < N; x++)
+                        {
+                            Console.Write("*");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("N is not odd");
+            }
+        }
     }
 }
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary;
+class Exersice
+{ 
+    static void Main()
+    {
+        int N = 0;
+        Console.WriteLine("Enter N: ");
+        N = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Exercise #1:");
+        ClassLibrary.Exercise1 exercise1 = new ClassLibrary.Exercise1();
+        exercise1.DoSequence(N);
+        Console.WriteLine("Exercise #2:");
+        ClassLibrary.Exercise2 exercise2 = new ClassLibrary.Exercise2();
+        exercise2.DoRectangle(N);
+    }
+}
